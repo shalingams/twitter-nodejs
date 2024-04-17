e0168dca61e0662c61397433258b58bf2b1249bf
Shalinga Manasinghe <shalingams@gmail.com>
2024-04-17 06:07:01 +0300

    adding loging controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,15 +1,49 @@
-import { createUser, getUserByUsernameOrEmail } from "../db/users";
-import express from 'express';
+import {
+  createUser,
+  getUserByEmail,
+  getUserByUsernameOrEmail,
+} from "../db/users";
+import express from "express";
 import { authentication, random } from "../helpers";
 
-export const register = async (req: express.Request, res: express.Response) => {
+export const login = async (req: express.Request, res: express.Response) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.sendStatus(400);
+  }
+
+  const user = await getUserByEmail(email).select(
+    "+authentication.salt +authentication.password"
+  );
+
+  if (!user) {
+    return res.sendStatus(400);
+  }
+
+  const expectedHash = authentication(user.authentication.salt, password);
+
+  if (user.authentication.password != expectedHash) {
+    return res.sendStatus(403);
+  }
+  const salt = random();
+
+  user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+  await user.save();
+
+  res.cookie('XIT-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+  
+  return res.status(200).json(user).end();
+};
+
+export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username, name } = req.body;
     if (!email || !password || !username || !name) {
       return res.sendStatus(400);
     }
-    
+
     const existingUser = await getUserByUsernameOrEmail(username, email);
     if (existingUser) {
       return res.sendStatus(400);
@@ -31,4 +65,4 @@ export const register = async (req: express.Request, res: express.Response) => {
     console.log(error);
     return res.sendStatus(400);
   }
-}
+};
